010d40a9efb9cb2cd5779315c72813f1955722a9
Untitled <gnu.imm@outlook.com>
2024-03-17 00:38:03 +0800

    all: added basic exports

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import universities from './list.json'
+import { University } from './types'
+
+
+export { RawUniversity, University } from './types'
+
+export default universities as University[]
---
+++ src/types.ts
@@ -0,0 +1,12 @@
+export interface RawUniversity {
+  name: string
+  domains: string[]
+  web_pages: string[]
+  country: string
+  alpha_two_code: string
+  'state-province': string | null
+}
+
+export interface University extends RawUniversity {
+  id: number
+}
