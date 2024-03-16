f9ff0ce1c203fcb98e9badc1856155d01b60ed44
Untitled <gnu.imm@outlook.com>
2024-03-17 00:38:18 +0800

    scripts: added basic list generation script

---
+++ src/scripts/gen.ts
@@ -0,0 +1,24 @@
+import { readFile, writeFile } from 'fs/promises'
+import path from 'path'
+import type { RawUniversity, University } from '../types'
+
+
+/**
+ * Generate a **new** list from the raw list provided by
+ * university-domains-list without considering any historical data
+ */
+async function main() {
+  const rawPath = path.resolve(__dirname, '../../university-domains-list/world_universities_and_domains.json')
+  const rawUniversities = JSON.parse((await readFile(rawPath)).toString()) as
+    RawUniversity[]
+  const list = rawUniversities.map((university, i) => ({
+    ...university,
+    id: i + 1
+  } satisfies University))
+  const outPath = path.resolve(__dirname, '../../src/list.json')
+  console.log('Generating new list at', outPath)
+  await writeFile(outPath, JSON.stringify(list, null, 2))
+  console.log('Generated new list at', outPath)
+}
+
+main()
