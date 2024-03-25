71e8e672969285e59b0fec92a07fd5b1aff42be5
Untitled <gnu.imm@outlook.com>
2024-03-26 03:23:27 +0800

    all: updated upstream and fixed abbreviations of universities in Hong Kong

--- src/scripts/helpers/guessAbbr.ts
+++ src/scripts/helpers/guessAbbr.ts
@@ -2,13 +2,24 @@ import assert from 'assert'
 import { check } from 'tlds2'
 
 
+const eduRegexp = /(?<org>[^./]+)\.(edu|ac)(\.[a-z]{2,3})?$/i
+
 export default function guessAbbr(url: string) {
   const _url = new URL(url)
   const { hostname } = _url
+  if (hostname.includes('.edu.') || hostname.includes('.ac.')) {
+    const org = guessAbbrFromEdu(hostname)
+    if (org) return org
+  }
   const res = check(hostname)
   if (typeof res.error === 'string') {
     throw new Error(`Failed to get organization from ${url}: ${res.error}`)
   }
   assert(res.org)
   return res.org
+}
+
+function guessAbbrFromEdu(hostname: string) {
+  const match = eduRegexp.exec(hostname)
+  return match?.groups?.org
 }
